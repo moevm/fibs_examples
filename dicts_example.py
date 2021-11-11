e4d68dc5c7b261aa19b3706dca0a46cd349bdb84
TatyanaBerlenko <tank.berlin@gmail.com>
2021-11-11 20:34:46 +0300

    Create dicts_example.py

---
+++ dicts_example.py
@@ -0,0 +1,9 @@
+countries = {
+    "Russia": "Moscow",
+    "Finland": "Helsinki",
+    "USA": "New York",
+    "USA": "Washington, D.C."
+}
+print(countries)
+print(type(countries))
+print(countries["Russia"])
